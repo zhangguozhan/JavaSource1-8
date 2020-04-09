483ab7c9e7de52cc873866134743f7b04e8f867c
zhangguozhan <1299355661@qq.com>
2020-04-09 20:38:40 +0800

    javasource

---
+++ src/ademo/MapTest.java
@@ -0,0 +1,18 @@
+package ademo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map<String, String> map = new HashMap<String, String>();
+		
+		map.put("hello", "world");
+		
+		System.out.println(map.toString());
+
+	}
+
+}
